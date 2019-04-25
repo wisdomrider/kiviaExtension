7d55f7ffad08c1fa1468abf5f4a3de5d7b4d1e5a
avishek <avishekzone@gmail.com>
2019-04-26 01:21:22 +0545

    new design

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+
+
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                var h = document.getElementsByTagName("html")[0]
+                h.insertAdjacentHTML("afterbegin", allText);
+                var ifra = document.createElement("script");
+                ifra.src = browser.runtime.getURL("kivia.js");
+                document.body.appendChild(ifra);
+
+
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+
 browser.runtime.onMessage.addListener(request => {
- 
+    x=document.getElementsByClassName("overlay")
+    if(x.length>0){
+        Array.from(x).forEach(x1=>{
+            x1.remove()
+        })
+    }
+    else
+        readTextFile(browser.runtime.getURL("index.html"))
+    // h.insertAdjacentHTML("afterbegin",x);
+    // console.log(x);
+
 });
--- kivia.js
+++ kivia.js
@@ -1,14 +1,51 @@
-var btn=document.getElementById("btn")
 var chat=document.getElementById("chat")
 var msg=document.getElementById("msg")
+var close=document.getElementById("close")
 
-keyPress=()=>{
+var ovrlay=document.getElementById("ovrlay1")
+
+ovrlay.addEventListener("click",()=>{})
+close.addEventListener("click",()=>{
+   Array.from(document.getElementsByClassName("overlay")).forEach(x=>{
+       x.remove()
+   })
+  
+})
+
+
+function keyPress(){
     if (window.event.keyCode==13){
-        chat.innerHTML+=msg.value+"<br>";
+        chat.innerHTML+='<div class="msg-right">'+msg.value+"</div>";
+        send(msg.value)
         msg.value=""    
+        
         chat.scrollTop = chat.scrollHeight;
-
     }
 }
 
 
+send=(message)=>{
+    var http = new XMLHttpRequest();
+    var url = 'https://dialogflow.googleapis.com/v2beta1/projects/quiznepall/agent/sessions/93d0756a-09a1-9baa-6e73-536ce8fb02bd:detectIntent';
+    var params = '{\"queryInput\":{\"text\":{\"text\":"'+message+'",\"languageCode\":\"en\"}},\"queryParams\":{\"timeZone\":\"Asia/Kathmandu\"}}';   
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    http.setRequestHeader('Authorization',"Bearer ya29.c.Elr2BjfCGp3NZR5AOJC3jJbdc_j9tCN2hztMT1a4S-Pw89TsamrA1WLHwjTwht8gqj8yy_fqsyqPMZnH17_FVuToSeWWL7konqCuIT6MRUm8oliVLGdJkbdc2fo")
+
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            var data=JSON.parse(http.responseText).queryResult.fulfillmentText;
+            chat.innerHTML+='<div class="msg-left">'+data+"</div>";
+            chat.scrollTop = chat.scrollHeight;
+        }
+        else if(http.status!=200&&http.readyState==4){   
+            chat.innerHTML+='<div class="msg-left">Unable to Connect !</div>';
+            chat.scrollTop = chat.scrollHeight;
+
+        }
+    }
+    http.send(params);
+}
+
+msg.focus()
