3059b26e526ce895bd3ac92cf2bef824791f5f26
Avishek <avishek@podamibenepal.com>
2019-04-26 15:24:25 +0545

    new updayes

--- kivia.js
+++ kivia.js
@@ -15,9 +15,16 @@ close.addEventListener("click",()=>{
 
 function keyPress(){
     if (window.event.keyCode==13){
+        if(msg.value.trim()=="clear")
+        {
+            chat.innerHTML=""
+            msg.value=""    
+            return;
+        } 
+    
         chat.innerHTML+='<div class="msg-right">'+msg.value+"</div>";
         send(msg.value)
-        msg.value=""    
+        msg.value=""
         
         chat.scrollTop = chat.scrollHeight;
     }
@@ -30,7 +37,7 @@ send=(message)=>{
     var params = '{\"queryInput\":{\"text\":{\"text\":"'+message+'",\"languageCode\":\"en\"}},\"queryParams\":{\"timeZone\":\"Asia/Kathmandu\"}}';   
     http.open('POST', url, true);
     http.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    http.setRequestHeader('Authorization',"Bearer ya29.c.Elr2BjfCGp3NZR5AOJC3jJbdc_j9tCN2hztMT1a4S-Pw89TsamrA1WLHwjTwht8gqj8yy_fqsyqPMZnH17_FVuToSeWWL7konqCuIT6MRUm8oliVLGdJkbdc2fo")
+    http.setRequestHeader('Authorization',"Bearer ya29.c.Elr3BjWRT2D06ZVUghMO-nWUEz9deRsfWkBalPAT4nBCds5aaPzpXBzUNU5lK-SV-sTNln6jFW425_1mDw9JVrfei6QMd5SD9M7B1wePnMYUl2RkXsaxUD0j3fw")
 
 
     http.onreadystatechange = function() {//Call a function when the state changes.
@@ -40,6 +47,7 @@ send=(message)=>{
             chat.scrollTop = chat.scrollHeight;
         }
         else if(http.status!=200&&http.readyState==4){   
+            alert(http.response)
             chat.innerHTML+='<div class="msg-left">Unable to Connect !</div>';
             chat.scrollTop = chat.scrollHeight;
 
